66ec6505011bc71adb4c56b8e24571babf03e930
smyslov <smyslov@5D80>
2019-01-23 21:48:50 +0300

    Добавьте файлы проекта.

---
+++ FindPair2/FormMain.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace FindPair2
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxField = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxField
+            // 
+            this.pictureBoxField.BackColor = System.Drawing.Color.White;
+            this.pictureBoxField.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxField.Name = "pictureBoxField";
+            this.pictureBoxField.Size = new System.Drawing.Size(232, 211);
+            this.pictureBoxField.TabIndex = 0;
+            this.pictureBoxField.TabStop = false;
+            this.pictureBoxField.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxField_MouseDown);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(510, 465);
+            this.Controls.Add(this.pictureBoxField);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Find Pair";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxField)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxField;
+    }
+}
+
---
+++ FindPair2/FormMain.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindPair2
+{
+    public partial class FormMain : Form
+    {
+        static Random rnd = new Random();
+
+        const char GUESSED_CELL = '!';//если отгадана
+        const char EMPTY_CELL = '.';//если отгадана
+
+        const int N = 6;//кол-во строк
+        const int M = 8;//кол-во столбцов
+        char[,] field = new char[N, M];//игровое поле
+        const int CELL_SIZE = 100;
+
+        int i1Click = -1, j1Click = -1;//координаты ячейки 1 клетки из пары
+        int i2Click = -1, j2Click = -1;//координаты ячейки 2 клетки из пары
+
+        int gameStep = 0;
+
+        Bitmap bitmap;
+        Graphics graphics;
+
+        private void ClearField()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    field[i, j] = EMPTY_CELL;
+                }
+            }
+        }
+
+        private void GenerateField()
+        {
+            int countPairs = N * M / 2;
+            for (int k = 0; k < countPairs; k++)
+            {
+                char currentLetter = (char)rnd.Next('А', 'Я' + 1);
+
+                for (int kk = 0; kk < 2; kk++)
+                {
+                    int i, j;
+                    do
+                    {
+                        i = rnd.Next(0, N);
+                        j = rnd.Next(0, M);
+                    }
+                    while (field[i, j] != EMPTY_CELL);
+                    field[i, j] = currentLetter;
+                }
+            }
+        }
+
+        private void DrawField()
+        {
+            graphics.Clear(Color.White);
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (field[i, j] != GUESSED_CELL)
+                    {
+                        graphics.DrawRectangle(new Pen(Color.Red), j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+                        if (i == i1Click && j == j1Click || i == i2Click && j == j2Click)
+                        {
+                            graphics.DrawString(field[i, j].ToString(), new Font("Arial", 20), new SolidBrush(Color.Black), j * CELL_SIZE + CELL_SIZE / 3, i * CELL_SIZE + CELL_SIZE / 3);
+                        }
+                    }
+                    else
+                    {
+                        graphics.FillRectangle(new SolidBrush(Color.Red), j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    }
+                }
+            }
+            pictureBoxField.Image = bitmap;
+            pictureBoxField.Refresh();
+        }
+
+        private bool IsWin()
+        {
+            int countGuessed = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if(field[i,j]==GUESSED_CELL)
+                    {
+                        countGuessed++;
+                    }
+                }
+            }
+            return countGuessed == N * M;   
+        }
+
+        private void pictureBoxField_MouseDown(object sender, MouseEventArgs e)
+        {
+            gameStep++;
+
+            if (gameStep == 1)
+            {
+                i1Click = e.Y / CELL_SIZE;
+                j1Click = e.X / CELL_SIZE;
+
+                DrawField();
+            }
+            else if (gameStep == 2)
+            {
+                i2Click = e.Y / CELL_SIZE;
+                j2Click = e.X / CELL_SIZE;
+
+                DrawField();
+                System.Threading.Thread.Sleep(500);
+
+                if (field[i1Click, j1Click] == field[i2Click, j2Click])
+                {
+                    field[i1Click, j1Click] = GUESSED_CELL;
+                    field[i2Click, j2Click] = GUESSED_CELL;
+                }
+                
+                i1Click = i2Click = j1Click = j2Click = -1;
+                
+                DrawField();
+
+                if(IsWin())
+                {
+                    pictureBoxField.Enabled = false;
+                    MessageBox.Show("Победа! Вы отгадали все буквы!");
+                }
+                    
+                gameStep = 0;
+            }
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            int width = CELL_SIZE * M + 1;
+            int height = CELL_SIZE * N + 1;
+
+            pictureBoxField.Width = width;
+            pictureBoxField.Height = height;
+
+            bitmap = new Bitmap(width, height);
+            graphics = Graphics.FromImage(bitmap);
+
+            ClearField();
+            GenerateField();
+            DrawField();
+        }
+    }
+}
